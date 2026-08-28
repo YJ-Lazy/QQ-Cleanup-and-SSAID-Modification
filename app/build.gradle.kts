e2b2d84cea8c938ede5807770b996185d97bb030
YJ-Lazy <yuanjuan52025@gmail.com>
2026-08-28 21:47:36 +0800

    chore: bump version to 0.1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.ace.toolbox"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "0.1.4"
+        versionCode = 15
+        versionName = "0.1.5"
 
         vectorDrawables.useSupportLibrary = true
     }
